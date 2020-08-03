b6db6633f49bd89bffd8713622dd794c3fe3af23
andiz <andiz@sfu.ca>
2020-08-03 13:22:33 -0700

    last

--- functions.cpp
+++ functions.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 // Set this to 'true' if you (the human) want to play against the AI.
-// Note: For human games, you will play against AI2. 
+// Note: For human games, you will play against "X" AI. 
 const bool IS_HUMAN_GAME = true;
 
-// Set this to 'true' if you want AI1 to use the modified MCTS algorithm
+// Set this to 'true' if you want the "O" AI to use the modified MCTS algorithm
 // Does nothing if IS_HUMAN_GAME is set to true
-const bool ADVANCED_AI1 = true;
+const bool ADVANCED_O_AI = true;
 
-// Set this to 'true' if you want AI2 to use the modified MCTS algorithm
+// Set this to 'true' if you want "X" AI to use the modified MCTS algorithm
 // This will be your opponent if you play a human game.
-const bool ADVANCED_AI2 = true;
+const bool ADVANCED_X_AI = true;
 
 int main() {
-    startGame(IS_HUMAN_GAME, ADVANCED_AI1, ADVANCED_AI2);
+    startGame(IS_HUMAN_GAME, ADVANCED_O_AI, ADVANCED_X_AI);
 }
